e54349f9b51f93fb938341794a90710145328ef4
CowdogEin <candats@outlook.com>
2025-04-06 20:12:36 +0800

    ### Audio

--- main.lua
+++ main.lua
@@ -46,6 +46,13 @@ function love.load()
     -- Set active font to the smallFont object
     love.graphics.setFont(SmallFont)
 
+    -- Audio Initialize
+    Sounds = {
+        ["paddle_hit"] = love.audio.newSource("sounds/paddle_hit.wav", "static"),
+        ["score"] = love.audio.newSource("sounds/score.wav", "static"),
+        ["wall_hit"] = love.audio.newSource("sounds/wall_hit.wav", "static")
+    }
+
     -- Initialize window with virtual resolution
     Push: setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
@@ -88,6 +95,8 @@ function love.update(dt)
             else
                 TheBall.dy = math.random(10, 150)
             end
+
+            Sounds["paddle_hit"]: play()
         end
 
         if TheBall: collides(Player2) then
@@ -99,22 +108,30 @@ function love.update(dt)
             else
                 TheBall.dy = math.random(10, 150)
             end
+
+            Sounds["paddle_hit"]: play()
         end
 
         if TheBall.y <= 0 then
             TheBall.y = 0
             TheBall.dy = - TheBall.dy
+
+            Sounds["wall_hit"]: play()
         end
 
         if TheBall.y >= VIRTUAL_HEIGHT - 4 then
             TheBall.y = VIRTUAL_HEIGHT - 4
             TheBall.dy = -TheBall.dy
+
+            Sounds["wall_hit"]: play()
         end
 
         -- Score
         if TheBall.x < 0 then
             ServingPlayer = 1
             Player2Score = Player2Score + 1
+
+            Sounds["score"]: play()
 
             if Player2Score == 5 then
                 WinningPlayer = 2
@@ -128,6 +145,8 @@ function love.update(dt)
         if TheBall.x > VIRTUAL_WIDTH then
             ServingPlayer = 2
             Player1Score = Player1Score + 1
+
+            Sounds["score"]: play()
 
             if Player1Score == 5 then
                 WinningPlayer = 1
